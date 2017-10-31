5cb2c2043cc0dfdd2eed652c4ad368c9877cf939
staker27 <tandm1327@gmail.com>
2017-10-31 16:34:04 +0900

    instance creation

--- ClassSample/ClassSample/Program.cs
+++ ClassSample/ClassSample/Program.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace ClassSample
 {
-    class Program
+    // Diceをふって出た目をチェックするというプログラムを考える
+    class Dice
     {
-        static void Main(string[] args)
+        private Random _rnd;　// フィールド
+        public Dice() // コンストラクタ
         {
+            int t = Environment.TickCount;
+            while (t == Environment.TickCount) {}
+            _rnd = new Random();
+            _value = 1;
+        }
+
+        private int _value; // フィールド
+        public int Value
+        {
+            // 上面の値
+            get
+            {
+                return _value;
+            }
+        }
+
+        public void Throw() // ダイスを振る動作
+        {
+            _value = _rnd.Next(6) + 1; // _valueがさいころの上面
+            // アクセス修飾子がprivateの変数は、アンスコをつけるのが慣習
+            // これはPythonの時も同じでした
         }
     }
-
-    // Diceをふって出た目をチェックするというプログラムを考える
 }
