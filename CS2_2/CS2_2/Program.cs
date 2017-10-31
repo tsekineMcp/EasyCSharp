dbe5991168fb3805bc9bcac3df610909d2ee525a
staker27 <tandm1327@gmail.com>
2017-10-31 12:52:55 +0900

    integrated repos

---
+++ CS2_2/CS2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int s = a + b;
+            string m = "{0}足す{1}は{2}です";
+            Console.WriteLine(m, a, b, s);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScopeSample/ScopeSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScopeSample
+{
+    class Program
+    {
+        private static int a = 1;
+        static void Main(string[] args)
+        {
+            int b = 2;
+            {
+                //　ステートメントブロック
+                //　ブロックがネストしている場合、親のブロックの変数などは使える
+                int s = a + b;
+                Console.WriteLine(s);
+            }
+
+            //　たとえば、ここでsを呼び出すことはできない（内側のブロックでライフサイクルが終わっている）
+
+            Console.ReadLine();
+        }
+    }
+}
