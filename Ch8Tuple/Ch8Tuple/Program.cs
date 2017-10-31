69f8a7126d62b47ae19c6bfcf2f0a6c1a1dd4915
staker27 <tandm1327@gmail.com>
2017-10-31 15:58:02 +0900

    tuple sample

---
+++ Ch8Tuple/Ch8Tuple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int, int) a = (1, 2);
+            Console.WriteLine(a.Item1);
+            Console.WriteLine(a.Item2);
+
+            (int x, int y) b = (1, 2);
+            Console.WriteLine(b.x);
+            Console.WriteLine(b.y);
+
+            var c = (x: 1, y: 2);
+            Console.WriteLine(c.x);
+            Console.WriteLine(c.y);
+        }
+    }
+}
