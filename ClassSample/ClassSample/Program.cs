28cdea2d640c2c4aeac48aaf54143d9b315c22af
staker27 <tandm1327@gmail.com>
2017-10-31 16:01:17 +0900

    start feature/dice

---
+++ ClassSample/ClassSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Diceをふって出た目をチェックするというプログラムを考える
+}
