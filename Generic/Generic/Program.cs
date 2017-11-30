51dae1480d7ac5283aa1be0cd407165fe79cdeed
tsekineMcp <takeru.sekine@mcp-am.com>
2017-11-30 11:30:18 +0900

    finish generics

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generic
+namespace Generics
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Swap<int>(ref a, ref b); // invoking Swap against int
+            Console.WriteLine("a={0}, b={1}", a, b);
+
+            string c = "右";
+            string d = "左";
+
+            Console.WriteLine("c={0}, d={1}", c, d);
+            Swap(ref c, ref d); // invoking Swap, but not declare a data type, in this case, C# guess the type.
+            Console.WriteLine("c={0}, d={1}", c, d);
+
+            Console.ReadLine();
+        }
+
+        static void Swap<T>(ref T v1, ref T v2)
+        {
+            T b = v1;
+            v1 = v2;
+            v2 = b;
         }
     }
+
 }
